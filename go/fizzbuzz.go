bcab4447354827ef046e1019414a079a9aa69805
Eric Bruno <eric@ebruno.org>
2021-03-21 18:43:17 -0700

    add go version

---
+++ go/fizzbuzz.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func fizzbuzz(start int, end int, step int, modulo_fizz int, modulo_buzz int) int {
+	status := 0
+	var result string = ""
+	for value := start; value < end; value += step {
+		result = ""
+		if (value % modulo_fizz) == 0 {
+		   result += "fizz"
+		}
+		if (value % modulo_buzz) == 0 {
+		   result += "buzz"
+		}
+		if len(result) == 0 {
+		  fmt.Printf("%d\n",value)
+		} else {
+		  fmt.Printf("%s\n",result)
+		}
+	}
+	return status
+}
+
+func main() {
+	exit_status := 0
+	start := 1
+	end := 100
+	step := 1
+	modulo_fizz := 3
+	module_buzz := 5
+	exit_status = fizzbuzz(start, end, step, modulo_fizz, module_buzz)
+	os.Exit(exit_status)
+}
