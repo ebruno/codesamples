9e628cd19cdd06bf7c7079374048f783bff4b649
Eric Bruno <eric@ebruno.org>
2025-09-12 05:01:26 +0000

    Podman support

--- k8s_kind/README.md
+++ k8s_kind/README.md
@@ -5,21 +5,18 @@ Demostration of a nginx server using [kind](https://kind.sigs.k8s.io) and k8.
 This has been tested on the following environments
 
  * Debian 12 requires kind, kubectl and docker to be installed on the system.
-
-
+ * RHEL 10 requires kind, kubectl and podman to be installed on the system.
 
 ## Running the demo ##
 
-     ./setup_cluster.sh
+	 ./setup_cluster.sh
 	 ./run_cluster.sh
-	 
+
 	 # When you are done.
-	 
+
 	 ./cleanup.sh
 
 
 Sample Run
 
 <img src="images/sample_run.png" alt="alt text" title="Sample Run">
- 
- 
--- k8s_kind/setup_cluster.sh
+++ k8s_kind/setup_cluster.sh
@@ -1,23 +1,31 @@
 #!/usr/bin/env bash
+KIND=$(which kind);
+DOCKER=$(which docker);
+PODMAN=$(which podman);
+KUBECTL=$(which kubectl);
 KIND_CLUSTER_NAME="k8s-nginx-cluster";
 IMAGE_NAME="k8_kind_nginx";
 IMAGE_TAG="latest";
-KIND=$(which kind);
-DOCKER=$(which docker)
+DOCKERFILE="web_server/Dockerfile";
+declare -i exit_status;
 if [ -x /usr/bin/sw_vers ]; then
     OS_TYPE="$(sw_vers -productName)";
 elif [ -f /etc/os-release ]; then
     . /etc/os-release
     OS_TYPE="${ID}";
 fi;
-
-if [ -n "${KIND}" ] && [ -n "${DOCKER}" ]; then
+if [ -n "${PODMAN}" ]; then
+    DOCKER="${PODMAN}";
+    IMAGE_NAME="localhost/k8_kind_nginx";
+    DOCKERFILE="web_server/Containerfile";
+fi;
+if [ -n "${KIND}" ] && [ -n "${DOCKER}" ] && [ -n "${KUBECTL}" ]; then
     ${KIND} delete cluster --name "${KIND_CLUSTER_NAME}"
     # See if image exits";
     if ${DOCKER} images | grep -q "${IMAGE_NAME}"; then
 	echo "[INFO] image ${IMAGE_NAME} exists" 1>&2;
     else
-    ${DOCKER} build -t "${IMAGE_NAME}:${IMAGE_TAG}" -f web_server/Dockerfile .
+    ${DOCKER} build -t "${IMAGE_NAME}:${IMAGE_TAG}" -f "${DOCKERFILE}" .
     fi;
     if ${KIND} get clusters | grep -q "${KIND_CLUSTER_NAME}"; then
 	echo "[INFO] Cluster ${KIND_CLUSTER_NAME} exists" 1>&2;
@@ -44,14 +52,16 @@ if [ -n "${KIND}" ] && [ -n "${DOCKER}" ]; then
 	echo "[ERROR] Image '${IMAGE_NAME}' is NOT present on the cluster."  1>&2;
 	exit 1
     fi;
-else
+    ${KUBECTL} cluster-info --context kind-${KIND_CLUSTER_NAME}
     if [ -z "${KIND}" ]; then
 	echo "[ERROR] kind is not installed on the system" 1>&2;
     fi;
     if [ -z "${DOCKER}" ]; then
 	echo "[ERROR] docker is not installed on the system" 1>&2;
-    fi;	
-    exit 1;
+    fi;
+    if [ -z "${KUBECTL}" ]; then
+	echo "[ERROR] kubectl is not installed on the system." 1>&2;
+    fi;
+    exit_status=1;
 fi;
-
-
+exit ${exit_status};
